4716521ab6cdb11734f6eb1b598d748fe220080e
Kurt Heiligmann <kurtheiligmann@gmail.com>
2015-01-17 23:05:59 -0800

    added CalendarViewController

--- iOS/FoodTruckr/FoodTruckr/CalendarViewController.h
+++ iOS/FoodTruckr/FoodTruckr/CalendarViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  CalendarViewController.h
 //  FoodTruckr
 //
 //  Created by Kurt Heiligmann on 1/17/15.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface CalendarViewController : UIViewController
 
 @end
-
---
+++ iOS/FoodTruckr/FoodTruckr/CalendarViewController.m
@@ -0,0 +1,21 @@
+//
+//  CalendarViewController.m
+//  FoodTruckr
+//
+//  Created by Kurt Heiligmann on 1/17/15.
+//  Copyright (c) 2015 Kurt. All rights reserved.
+//
+
+#import "CalendarViewController.h"
+
+@interface CalendarViewController () <UITableViewDelegate, UITableViewDataSource>
+@property (nonatomic, weak) IBOutlet UITableView *truckTable;
+@end
+
+@implementation CalendarViewController
+
+#pragma mark - UITableViewDelegate
+
+#pragma mark - UITableViewDataSource
+
+@end
