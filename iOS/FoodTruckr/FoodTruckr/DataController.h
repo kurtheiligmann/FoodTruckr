7917006b397a4d381476669e51dfd73fdc24b8c1
Kurt Heiligmann <kurtheiligmann@gmail.com>
2015-01-17 23:17:12 -0800

    added DataController

---
+++ iOS/FoodTruckr/FoodTruckr/DataController.h
@@ -0,0 +1,13 @@
+//
+//  DataController.h
+//  FoodTruckr
+//
+//  Created by Kurt Heiligmann on 1/17/15.
+//  Copyright (c) 2015 Kurt. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DataController : NSObject
+
+@end
---
+++ iOS/FoodTruckr/FoodTruckr/DataController.m
@@ -0,0 +1,13 @@
+//
+//  DataController.m
+//  FoodTruckr
+//
+//  Created by Kurt Heiligmann on 1/17/15.
+//  Copyright (c) 2015 Kurt. All rights reserved.
+//
+
+#import "DataController.h"
+
+@implementation DataController
+
+@end
