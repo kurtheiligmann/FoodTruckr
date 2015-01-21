8e9e985ab2b9e9a5120e4d8c3837195d9033f2a1
Brian Melton <iambmelton@gmail.com>
2015-01-21 07:49:25 -0800

    Closes # 18

--- Android/FoodTruckr/app/src/main/java/com/brianjmelton/foodtruckr/view/AbstractViews.java
+++ Android/FoodTruckr/app/src/main/java/com/brianjmelton/foodtruckr/view/AbstractViews.java
@@ -5,9 +5,16 @@ import android.view.View;
 /**
  * Created by brianmelton on 1/17/15.
  *
+ * Abstract super class for {@link android.app.Fragment} ViewContainer pattern
  */
 public abstract class AbstractViews {
 
+    /**
+     * Initialize {@link android.app.Fragment} {@link android.view.View}s here
+     *
+     * @param layout the root layout
+     * @return the root layout
+     */
     protected abstract View initialize(View layout);
 
 }
